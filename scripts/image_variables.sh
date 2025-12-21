5d48a6a8ca08c5a6af97513d35102e773ee9363f
Dan Herman <dan.herman@me.com>
2025-12-21 17:52:40 -0500

    updated script

--- scripts/image_variables.sh
+++ scripts/image_variables.sh
@@ -2,12 +2,15 @@
 export $(cat .env | grep -v '#' | awk '/=/ {print $1}')
 
 # 2. Define the tag of the EXISTING images
-OLD_TAG="v20251220-000829"
+OLD_TAG="v20251221-213400"
 REPO=${REPO_NAME:-ml-pipelines} # Default to ml-pipelines if not set
 
 # 3. Set the URIs for the images you want to SKIP building
-export TENSORFLOW_IMAGE_URI="us-east1-docker.pkg.dev/${PROJECT_ID}/${REPO}/tensorflow-training:${OLD_TAG}"
-export PYTORCH_SERVING_IMAGE_URI="us-east1-docker.pkg.dev/${PROJECT_ID}/${REPO}/pytorch-serving:${OLD_TAG}"
+# export TENSORFLOW_IMAGE_URI="us-east1-docker.pkg.dev/${PROJECT_ID}/${REPO}/tensorflow-training:${OLD_TAG}"
+
+export PYTORCH_IMAGE_URI="us-east1-docker.pkg.dev/${PROJECT_ID}/${REPO}/pytorch-serving:${OLD_TAG}"
+
+export PYTORCH_SERVING_IMAGE_URI="us-east1-docker.pkg.dev/${PROJECT_ID}/${REPO_NAME}/${PYTORCH_IMAGE_NAME}:${OLD_TAG}"
 
 # 4. Ensure the one you want to BUILD is unset (so it generates a new tag/URI)
-unset PYTORCH_IMAGE_URI
+unset TENSORFLOW_IMAGE_URI
